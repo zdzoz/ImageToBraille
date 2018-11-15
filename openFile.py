c9046636a4125bd8abdbeafbec6828784aed3eea
zdzoz <oskarzdz@gmail.com>
2018-11-14 22:38:08 -0800

    update image downloader with random name

--- openFile.py
+++ openFile.py
@@ -1,3 +1,12 @@
-import urllib
+import urllib.request
+import string
+import random
 
-urllib.urlretrieve("https://s1.narvii.com/image/ws45li53clncpxdjivdk7o3wars77rxa_hq.jpg", "OMEGA.jpg")
+def downloader(image_url):
+    file_name = random.randrange(1,10000)
+    random_str = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    full_file_name = random_str + str(file_name) + '.png'
+    urllib.request.urlretrieve(image_url,full_file_name)
+
+
+downloader("https://s1.narvii.com/image/ws45li53clncpxdjivdk7o3wars77rxa_hq.jpg")
