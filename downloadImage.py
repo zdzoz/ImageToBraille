e364ac4f1cf39997c0659f9a516d75ae93dc2835
zdzoz <oskarzdz@gmail.com>
2018-11-14 23:07:05 -0800

    make pics go into pics directory

--- downloadImage.py
+++ downloadImage.py
@@ -1,9 +1,12 @@
 import urllib.request
 import string
 import random
+import os
 
 def downloader(image_url):
     file_name = random.randrange(1,10000)
     random_str = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
     full_file_name = random_str + str(file_name) + '.png'
-    urllib.request.urlretrieve(image_url,full_file_name)
+    if not os.path.isdir("pics"):
+        os.mkdir("pics")
+    urllib.request.urlretrieve(image_url, os.path.join("pics", full_file_name))
